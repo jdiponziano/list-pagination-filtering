0bf038bdb73517436f96d807aa66efd09ecd9e63
Jessica Di Ponziano <jdiponziano@gmail.com>
2019-01-07 20:25:50 -0500

    Add search box

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ List Filter and Pagination
 document.addEventListener('DOMContentLoaded', () => {
   //Declare program variables
   const pageContainer = document.querySelector('.page');
+  const pageHeader = document.querySelector('.page-header');
   const classList = document.querySelector('.student-list');
   const students = classList.children;
 
-  const div = document.createElement('div');
   const ul = document.createElement('ul');
 
   //Show/hide page items in relation to page selected
@@ -25,6 +25,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  const createSearchForm = () => {
+    const div = document.createElement('div');
+    div.classList.add('student-search');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Search for students...';
+    div.appendChild(input);
+    const button = document.createElement('button');
+    button.textContent = 'Search';
+    div.appendChild(button);
+    pageHeader.appendChild(div);
+  }
+
   //Construct html of pagination link
   const createLink = text => {
     const li = document.createElement('li');
@@ -38,6 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //Add pagination links to page
   const appendPageLinks = (list) => {
     const pages = Math.ceil(list.length / 10);
+    const div = document.createElement('div');
     div.classList.add('pagination');
     for (let i = 1; i <= pages; i++) {
       createLink(i);
@@ -47,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //Initialize pagination on page load
+  createSearchForm();
   showPage(students, 1);
   appendPageLinks(students);
 
@@ -54,14 +69,16 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.pagination').addEventListener('click', (e) => {
     e.preventDefault();
     const activeLink = e.target;
+    if(activeLink.tagName === 'A') {
     const pageNumber = parseInt(activeLink.textContent);
     const links = document.querySelectorAll('.pagination a');
     showPage(students, pageNumber);
-    for(let i = 0; i < links.length; i ++) {
-      if ( links[i] == activeLink) {
-        links[i].classList.add('active');
-      } else {
-        links[i].classList.remove('active');
+      for(let i = 0; i < links.length; i ++) {
+        if ( links[i] == activeLink) {
+          links[i].classList.add('active');
+        } else {
+          links[i].classList.remove('active');
+        }
       }
     }
   })
